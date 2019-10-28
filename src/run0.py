7940d8257362e70faadf7aeeb7a798847db7542d
JD Astudillo <jdiego@umich.edu>
2019-10-28 14:50:30 -0700

    Update run0.py

--- src/run0.py
+++ src/run0.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 import csv
+import os
+ 
+dirpath = os.getcwd()
+print("current directory is : " + dirpath)
 
-with open('/Users/jdastu/Documents/GitHub/de-challenge-oct28/insight_testsuite/tests/test1/input/Border_Crossing_Entry_Data.csv') as csvfile:
+
+with open(os.path.join(dirpath, '../input/small_Border_Crossing_Entry_Data.csv')) as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     for row in readCSV:
         print(row)
-        print(row[0])
-        print(row[0],row[1],row[2],)
+        
