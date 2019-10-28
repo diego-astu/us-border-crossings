7f276e85a5f456a2e39d9bddac5c602841a6317d
JD Astudillo <jdiego@umich.edu>
2019-10-28 14:24:43 -0700

    initial commit?
    d

--- src/run0.py
+++ src/run0.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+import csv
+
+with open('example.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    for row in readCSV:
+        print(row)
+        print(row[0])
+        print(row[0],row[1],row[2],)
