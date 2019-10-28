615a891d81cea23bb59672df9dba75942d0edb0d
JD Astudillo <jdiego@umich.edu>
2019-10-28 14:27:57 -0700

    ynowork

--- src/run0.py
+++ src/run0.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import csv
 
-with open('example.csv') as csvfile:
+with open('/Users/jdastu/Documents/GitHub/de-challenge-oct28/insight_testsuite/tests/test1/input/Border_Crossing_Entry_Data.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     for row in readCSV:
         print(row)
