9fd21891bb4d99fa59c20d886ad4ebc0163391a6
JD Astudillo <jdiego@umich.edu>
2019-10-31 23:41:58 -0700

    solved expand grid

--- src/run0.py
+++ src/run0.py
@@ -17,15 +17,14 @@ crossing and border.
 Border_Crossing_Entry_Data.csv 
 ALl columns will be read as strings, but can be converted
 COLUMNS : 
-	Port Name
-	State
-	Port Code
-	Border
-	Date
-	Measure
-	Value
+	Port Name (ignore)
+	State (ignore)
+	Port Code (ignore)
+	Border (keep)
+	Date (keep)
+	Measure (keep)
+	Value (keep)
 	Location
-
 
 
 
@@ -81,64 +80,121 @@ import itertools
 import datetime
 import locale
 import collections
+import re
+from dateutil.relativedelta import relativedelta
 
 
-# Set some global preferences and define custom function(s)
-locale.setlocale(locale.LC_TIME, "en_US") # make sure that datetime output will be in US locale (AM/PM)
+
+#Set global locale preference to US locale
+locale.setlocale(locale.LC_TIME, "en_US") 
 def PrintNRows(iterable_object, Nrows = 4):
 	for i, row in enumerate(iterable_object):
 		print(i,row)
 		if (i >= (Nrows-1)):
 			break
 
-# Define some global variables literals
+
+def CleanWhitespace(string_in):
+	#convert all contiguous whitespace to be single space 
+	#remove leading and trailing whitespace,
+	cleaned_string = re.sub(r'\s+', ' ', string_in).strip()
+	return cleaned_string
+
+def StringToFloat(string_in):
+	#impute missing values to zero
+	#else convert to float
+	if CleanWhitespace(string_in) == '':
+		return 0.00
+	else:
+		return float(string_in)
+	return string_as_float
+
+def DateToString(datetime_in):
+    		return datetime_in.strftime("%m/%d/%Y %I:%M:%S %p")
+
+# Define  global variables
 input_filepath = os.path.join(os.getcwd(), '../input/small_Border_Crossing_Entry_Data.csv')
-keep_only_these_vars = ['Border','Date','Measure','Value']
 
 #########
-#STEP 1 : read in dataset, collapse timestamps to yearmonth values, keep only necessary columns using keep_only_these_vars
+#STEP 1 : read in dataset, 
+#make sure data conforms to desired formats
+#keep track of unique values of border,measure
+#keep track of min/max date 
 #########
-indat0 = [] #input data version 0
+input0 = [] #input data 
+unique_values_border, unique_values_measure, unique_values_date = set(), set(), set()
 with open(input_filepath) as csvfile:
-    reader = csv.DictReader(csvfile
-    	#, fieldnames=('Port Name','State','Port Code','Border','Date','Measure','Value','Location')
-    	)
+    reader = csv.DictReader(csvfile)
     for row in reader:
-    	row['Value'] = float(row['Value'])*1.00
     	# convert the Date variable which is a string to be a datetime, in 12-hour format
-    	yearmonthdaysecond_as_datetime = datetime.datetime.strptime(row['Date'], "%m/%d/%Y %I:%M:%S %p")
-    	#collapse that datetime into a yearmonth, set at midnight of the first of the month
-    	yearmonth_as_datetime = datetime.datetime(
-    		year = yearmonthdaysecond_as_datetime.year,
-    		month = yearmonthdaysecond_as_datetime.month,
+  		# allow for irregularity in whether we receive datetime or just date
+    	yearmonth_as_datetime0 = datetime.datetime.strptime(row['Date'][:10], "%m/%d/%Y")
+    	#collapse that date into a yearmonth, set at midnight of the first of the month
+    	yearmonth_as_datetime1 = datetime.datetime(
+    		year = yearmonth_as_datetime0.year,
+    		month = yearmonth_as_datetime0.month,
     		day = 1, 
     		hour = 0,
     		minute = 0,
     		second = 0)
-    	yearmonth_as_string = yearmonth_as_datetime.strftime("%m/%d/%Y %I:%M:%S %p")
-    	row['Date'] = yearmonth_as_string
-    	#when I convert yearmonth_as_datetime back to string, it must MATCH original Date string else break
-    	"""
-    	if row['Date'] != yearmonth_as_datetime.strftime("%m/%d/%Y %I:%M:%S %p"):
-    			print("SOMETHING IS UP W YOUR DATETIMES BRO: these must match and they dont")
-    			print("this is original row[date]")
-    			print(row['Date'])
-    			print("this is original converted to datestamp collapsed to yearmonth")
-    			break
-    	print(row['Date'])
-    	print(yearmonthdaysecond_as_datetime)
-    	print(yearmonthdaysecond_as_datetime.strftime("%m/%d/%Y %I:%M:%S %p"))
-    	print(yearmonth_as_datetime)
-    	print(yearmonth_as_string)
-    	print('***')
-    	"""
-    	indat0.append({ keptvar: row[keptvar] for keptvar in (keep_only_these_vars +[]) }) 
+    	
+    	#reformat for appending into input0
+    	yearmonth_out = DateToString(yearmonth_as_datetime1)
+    	border_out = CleanWhitespace(row['Border'])
+    	measure_out = CleanWhitespace(row['Measure'])
+
+
+    	#add to set if unique value
+    	unique_values_border.add(border_out)
+    	unique_values_date.add(yearmonth_as_datetime1)
+        unique_values_measure.add(measure_out)
+        
+    	
+    	#prepare output: keep only border, date, measure, and value
+    	output_keys = ['Border','Date','Measure','Value']
+    	output_values = [border_out,
+    					yearmonth_out,
+    					measure_out,
+    					StringToFloat(row['Value'])
+    					]
+
+    	#add rows to input0
+    	input0.append(dict(zip(output_keys,output_values))) 
+
+
+
+
 
 
 
 #########
-#STEP 2 : PAD MY DATASET WITH ZEROS TO ENSURE NUMBER OF ROWS
+#STEP 2 : PAD MY DATASET WITH ZEROS WHERE NECESSARY
 #########
+#Before summarizing, data must contain one row per border*measure*month
+#If a given combination doesnt exist in data, must pad with zeros.
+
+
+date_range = []
+lastmonth = max(unique_values_date)
+firstmonth = min(unique_values_date)
+
+while firstmonth <= lastmonth:
+    date_range.append(DateToString(firstmonth))
+    firstmonth += relativedelta(months=1)
+
+
+cartesian_product_unique_values = itertools.product(unique_values_border,unique_values_measure,date_range)
+list_unique_tuples = list(cartesian_product_unique_values)
+
+list_of_dicts_unique_combos = []
+for tup in list_unique_tuples:
+	dict_out = {'Border':tup[0], 'Measure':tup[1], 'Date':tup[2]}
+	list_of_dicts_unique_combos.append(dict_out)
+
+
+#print(list(unique_values_border))
+#print(list(itertools.combinations(list_of_unique_sets,r=2)))
+print(list_of_dicts_unique_combos)
 
 
 
@@ -146,27 +202,18 @@ with open(input_filepath) as csvfile:
 #STEP 3 : CREATE A NESTED FOR-LOOP FOR SUMMARY STATISTICS
 #########
 
-indat1 = sorted(indat0, key=operator.itemgetter('Border', 'Measure', 'Date'))
+sorted_input = sorted(input0, key=operator.itemgetter('Border', 'Measure', 'Date'))
 print("******PRINT UNSORTED FIRST FEW ROWS******")
-PrintNRows(indat0, Nrows = 10)
+PrintNRows(input0, Nrows = 10)
 print("******PRINT SORTED FIRST FEW ROWS******")
-PrintNRows(indat1,10)
+PrintNRows(sorted_input,10)
 
-print("for each border*measure*date, figure out how many crossings there were")
+
+
+
+print("within each border*measure, figure out how many crossings there were per date and moving average")
 host_data = []
-for k,v in itertools.groupby(indat1, key=lambda x:({'Border':x['Border'], 'Measure':x['Measure'], 'Date': x['Date']})):
-	returndict = {'sum':sum(row['Value'] for row in v)}
-	returnsNone =  returndict.update(k)
-	host_data.append(returndict)
-
-PrintNRows(host_data,10)
-
-
-
-
-print("for each border*measure, figure out how many crossings there were")
-host_data = []
-for i,j in itertools.groupby(indat1, key=lambda x:(x['Border'], x['Measure'])):
+for i,j in itertools.groupby(sorted_input, key=lambda x:(x['Border'], x['Measure'])):
 	running_total_previous_month = 0
 	index_this_month = 1
 	for k,l in itertools.groupby(j, key=lambda x:(x['Date'])):
@@ -175,11 +222,12 @@ for i,j in itertools.groupby(indat1, key=lambda x:(x['Border'], x['Measure'])):
 		else:
 			moving_average = int(round(running_total_previous_month/(index_this_month-1)))
 		total_this_month = sum(row['Value'] for row in l)
-		newrow = {'grouping':(i,k), 'sum':total_this_month,'moving_avg': moving_average}
-		host_data.append(newrow)
+		returndict = {'sum':total_this_month, 'moving_average':moving_average}
+
+		print(type(i))
+		returndict = {'Border':i[0], 'Measure':i[1], 'Date':k, 'sum':total_this_month,'moving_avg': moving_average}
+		host_data.append(returndict)
 		index_this_month = index_this_month + 1
 		running_total_previous_month = running_total_previous_month + total_this_month
 
-PrintNRows(host_data,10)
-
-print(type(host_data[1]['grouping']))
+#PrintNRows(host_data,10)
